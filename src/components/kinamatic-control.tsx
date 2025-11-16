004f41d7ffb6553efe476631f7a5d054e33e612e
harshith <harshith.ashvi@gmail.com>
2025-11-16 08:51:49 +0530

    add basic components, add required packages and update ui

---
+++ src/components/kinamatic-control.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const KinematicControl = () => {
+  return (
+    <div className="h-full flex-2">
+      <div className="w-full h-full bg-neutral-50 dark:bg-neutral-800 rounded-lg" />
+    </div>
+  );
+};
+
+export default KinematicControl;
