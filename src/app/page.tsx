1afaabd1fb4c86dae17990568d97e94f05618738
harshith <harshith.ashvi@gmail.com>
2025-11-17 20:46:17 +0530

    add built by text and minor ui change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import Navbar from "@/components/navbar";
 import KinematicCanvas from "@/components/kinamatic-canvas";
 import KinematicControl from "@/components/kinamatic-control";
@@ -14,10 +16,20 @@ export default function Home() {
     >
       <div className="max-w-7xl w-full mx-auto z-10 p-4">
         <Navbar />
-        <div className="flex items-start w-full gap-4 mt-4 h-[calc(100vh-120px)]">
+        <div className="flex items-start w-full gap-4 mt-4 h-[calc(100vh-130px)]">
           <KinematicCanvas />
           <KinematicControl />
         </div>
+        <p className="font-bold text-md text-neutral-800 dark:text-neutral-300 text-center mt-2">
+          Built by{" "}
+          <Link
+            href="https://x.com/HarshithAshvi"
+            target="_blank"
+            className="bg-clip-text text-transparent bg-linear-to-r from-blue-500 via-violet-500 to-blue-500"
+          >
+            Harshith
+          </Link>
+        </p>
       </div>
     </div>
   );
