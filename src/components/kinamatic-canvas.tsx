f66d95c925582058485cf101c7604dedb86502a8
harshith <harshith.ashvi@gmail.com>
2025-11-18 21:06:10 +0530

    add kinematic arm class and draw path, add animation of arms created

--- src/components/kinamatic-canvas.tsx
+++ src/components/kinamatic-canvas.tsx
@@ -1,9 +1,71 @@
-import React from "react";
+"use client";
+
+import { drawPath, KinematicArm } from "@/lib/kinamatic-arm";
+import { useEffect, useRef } from "react";
 
 const KinematicCanvas = () => {
+  const canvasRef = useRef<null | HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+    const path: { x: number; y: number }[] = [];
+    if (canvas && context) {
+      const dpr = window.devicePixelRatio || 1;
+      const rect = canvas.getBoundingClientRect();
+
+      canvas.width = rect.width * dpr;
+      canvas.height = rect.height * dpr;
+
+      const width = canvas.width;
+      const height = canvas.height;
+
+      const arm = new KinematicArm(width / 2, height / 2, 200, 0);
+      const arm2 = new KinematicArm(arm.getEndX(), arm.getEndY(), 200, 1.3);
+      const arm3 = new KinematicArm(arm2.getEndX(), arm2.getEndY(), 200, 1.3);
+
+      let angle = 0;
+
+      arm2.parent = arm;
+      arm3.parent = arm2;
+
+      updateArm();
+
+      function updateArm() {
+        if (!context) return;
+        context.clearRect(0, 0, width, height);
+        arm.angle = Math.sin(angle) * 1.2;
+        arm2.angle = arm.angle * 4;
+        arm3.angle = arm2.angle * 4;
+
+        arm2.x = arm.getEndX();
+        arm2.y = arm.getEndY();
+        arm3.y = arm2.getEndY();
+        arm3.x = arm2.getEndX();
+        angle += 0.001;
+
+        const endX = arm3.getEndX();
+        const endY = arm3.getEndY();
+        path.push({ x: endX, y: endY });
+        if (path.length > 10000) path.shift();
+
+        drawPath(context, path);
+
+        arm.renderArm(context as CanvasRenderingContext2D);
+        arm2.renderArm(context as CanvasRenderingContext2D);
+        arm3.renderArm(context as CanvasRenderingContext2D);
+
+        requestAnimationFrame(updateArm);
+      }
+    }
+  }, []);
+
   return (
     <div className="h-full flex-6">
-      <canvas className="w-full h-full bg-neutral-50 dark:bg-neutral-800 rounded-lg" />
+      <canvas
+        ref={canvasRef}
+        className="w-full h-full bg-neutral-50 dark:bg-neutral-800 rounded-lg"
+      />
     </div>
   );
 };
---
+++ src/lib/kinamatic-arm.ts
@@ -0,0 +1,72 @@
+export class KinematicArm {
+  x: number;
+  y: number;
+  length: number;
+  angle: number;
+  parent: null | KinematicArm;
+  constructor(x: number, y: number, length: number, angle: number) {
+    this.x = x;
+    this.y = y;
+    this.length = length;
+    this.angle = angle;
+    this.parent = null;
+  }
+
+  createArm(x: number, y: number, length: number, angle: number): KinematicArm {
+    return new KinematicArm(x, y, length, angle);
+  }
+
+  init(x: number, y: number, length: number, angle: number): void {
+    this.x = x;
+    this.y = y;
+    this.length = length;
+    this.angle = angle;
+  }
+
+  getEndX() {
+    let angle = this.angle;
+    let parent = this.parent;
+    while (parent) {
+      angle += parent.angle;
+      parent = parent.parent;
+    }
+    return this.x + Math.cos(angle) * this.length;
+  }
+
+  getEndY() {
+    let angle = this.angle;
+    let parent = this.parent;
+    while (parent) {
+      angle += parent.angle;
+      parent = parent.parent;
+    }
+    return this.y + Math.sin(angle) * this.length;
+  }
+
+  renderArm(context: CanvasRenderingContext2D) {
+    context.lineWidth = 5;
+    context.strokeStyle = "#000";
+
+    context.beginPath();
+    context.moveTo(this.x, this.y);
+    context.lineTo(this.getEndX(), this.getEndY());
+    context.stroke();
+  }
+}
+
+export function drawPath(
+  ctx: CanvasRenderingContext2D,
+  path: { x: number; y: number }[]
+) {
+  if (path.length < 2) return;
+
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 3;
+
+  ctx.beginPath();
+  ctx.moveTo(path[0].x, path[0].y);
+  for (let i = 1; i < path.length; i++) {
+    ctx.lineTo(path[i].x, path[i].y);
+  }
+  ctx.stroke();
+}
