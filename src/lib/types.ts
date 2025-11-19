c78d6c2c2091940bb62f9943a90d5437c656ce10
harshith <harshith.ashvi@gmail.com>
2025-11-19 21:28:49 +0530

    add label ui, kinematic control component for form, add animation layout component and types

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+export interface GeneralValueInterface {
+  initialAngle: number;
+  incrementAngle: number;
+  bgColor: string;
+  pathColor: string;
+}
+
+export interface ArmValueInterface {
+  length: number;
+  initialAngle: number;
+  incrementAngle: number;
+}
