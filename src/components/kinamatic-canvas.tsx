14a15b0247a473754990c28246655161d7c9d8c8
harshith <harshith.ashvi@gmail.com>
2025-11-20 20:12:29 +0530

    draw arms and animate based on the input values

--- src/components/kinamatic-canvas.tsx
+++ src/components/kinamatic-canvas.tsx
@@ -2,8 +2,17 @@
 
 import { useEffect, useRef } from "react";
 import { drawPath, KinematicArm } from "@/lib/kinamatic-arm";
+import { ArmValueInterface, GeneralValueInterface } from "@/lib/types";
 
-const KinematicCanvas = () => {
+interface KinematicCanvasProps {
+  generalValues: GeneralValueInterface;
+  armsValue: ArmValueInterface[];
+}
+
+const KinematicCanvas = ({
+  generalValues,
+  armsValue,
+}: KinematicCanvasProps) => {
   const canvasRef = useRef<null | HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -20,45 +29,72 @@ const KinematicCanvas = () => {
       const width = canvas.width;
       const height = canvas.height;
 
-      const arm1 = new KinematicArm(width / 2, height / 2, 120, 0);
-      const arm2 = new KinematicArm(arm1.getEndX(), arm1.getEndY(), 90, 0);
-      const arm3 = new KinematicArm(arm2.getEndX(), arm2.getEndY(), 70, 0);
+      // const arm1 = new KinematicArm(width / 2, height / 2, 120, 0);
+      // const arm2 = new KinematicArm(arm1.getEndX(), arm1.getEndY(), 90, 0);
+      // const arm3 = new KinematicArm(arm2.getEndX(), arm2.getEndY(), 70, 0);
 
-      let angle = 0;
+      const arms: KinematicArm[] = [];
 
-      arm2.parent = arm1;
-      arm3.parent = arm2;
+      armsValue.forEach((armValue, index) => {
+        const arm = new KinematicArm(
+          index === 0 ? width / 2 : arms[index - 1].getEndX(),
+          index === 0 ? height / 2 : arms[index - 1].getEndY(),
+          armValue.length,
+          armValue.initialAngle
+        );
+        arm.parent = index === 0 ? null : arms[index - 1];
+        arms.push(arm);
+      });
+
+      let angle = generalValues.initialAngle;
+
+      // arm2.parent = arm1;
+      // arm3.parent = arm2;
 
       updateArm();
 
       function updateArm() {
         if (!context) return;
         context.clearRect(0, 0, width, height);
-        arm1.angle = Math.sin(angle * 1.0) * 1.1;
-        arm2.angle = Math.cos(angle * 1.333) * 0.95;
-        arm3.angle = Math.sin(angle * 1.997) * 0.82;
+        // arm1.angle = Math.sin(angle * 1.0) * 1.1;
+        // arm2.angle = Math.cos(angle * 1.333) * 0.95;
+        // arm3.angle = Math.sin(angle * 1.997) * 0.82;
 
-        arm2.x = arm1.getEndX();
-        arm2.y = arm1.getEndY();
-        arm3.y = arm2.getEndY();
-        arm3.x = arm2.getEndX();
-        angle += 0.04;
+        // arm2.x = arm1.getEndX();
+        // arm2.y = arm1.getEndY();
+        // arm3.y = arm2.getEndY();
+        // arm3.x = arm2.getEndX();
 
-        const endX = arm3.getEndX();
-        const endY = arm3.getEndY();
+        arms.forEach((arm, index) => {
+          arm.angle = Math.sin(angle * 2) * 4;
+          if (index !== 0) {
+            arm.x = arms[index - 1].getEndX();
+            arm.y = arms[index - 1].getEndY();
+          }
+        });
+        // angle += 0.04;
+        angle += generalValues.incrementAngle;
+
+        if (arms.length === 0) return;
+        const endX = arms[arms.length - 1].getEndX();
+        const endY = arms[arms.length - 1].getEndY();
         path.push({ x: endX, y: endY });
         if (path.length > 10000) path.shift();
 
         drawPath(context, path);
 
-        arm1.renderArm(context as CanvasRenderingContext2D);
-        arm2.renderArm(context as CanvasRenderingContext2D);
-        arm3.renderArm(context as CanvasRenderingContext2D);
+        arms.forEach((arm) => {
+          arm.renderArm(context as CanvasRenderingContext2D);
+        });
+
+        // arm1.renderArm(context as CanvasRenderingContext2D);
+        // arm2.renderArm(context as CanvasRenderingContext2D);
+        // arm3.renderArm(context as CanvasRenderingContext2D);
 
         requestAnimationFrame(updateArm);
       }
     }
-  }, []);
+  }, [generalValues, armsValue]);
 
   return (
     <div className="h-full flex-6">
